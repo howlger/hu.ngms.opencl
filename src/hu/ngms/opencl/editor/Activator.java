74aff0626dc030e4de294d6641ec742f02ce77b0
Attila Sragli <sragli.attila@gmail.com>
2014-11-30 15:04:12 +0100

    Created a CODAN checker for memory space-related errors

--- src/hu/ngms/opencl/editor/Activator.java
+++ src/hu/ngms/opencl/editor/Activator.java
@@ -12,8 +12,7 @@ import org.osgi.framework.BundleContext;
  */
 public class Activator extends AbstractUIPlugin {
 
-	// The plug-in ID
-	public static final String PLUGIN_ID = "opencl-validator"; //$NON-NLS-1$
+	public static final String PLUGIN_ID = "opencl-editor"; //$NON-NLS-1$
 
 	// The shared instance
 	private static Activator plugin;
