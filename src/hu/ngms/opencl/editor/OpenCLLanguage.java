d767dfe903c20e33c3afe12637e2ab64d687061f
Attila Sragli <sragli.attila@gmail.com>
2015-01-23 14:09:12 +0100

    Working on programmatically defining symbols

--- src/hu/ngms/opencl/editor/OpenCLLanguage.java
+++ src/hu/ngms/opencl/editor/OpenCLLanguage.java
@@ -2,6 +2,7 @@ package hu.ngms.opencl.editor;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.stream.Stream;
 
 import org.eclipse.cdt.core.dom.ast.gnu.c.GCCLanguage;
@@ -9,6 +10,7 @@ import org.eclipse.cdt.core.language.settings.providers.ILanguageSettingsProvide
 import org.eclipse.cdt.core.model.ICLanguageKeywords;
 import org.eclipse.cdt.core.settings.model.ICConfigurationDescription;
 import org.eclipse.cdt.core.settings.model.ICLanguageSettingEntry;
+import org.eclipse.cdt.core.settings.model.util.CSettingEntryFactory;
 import org.eclipse.core.resources.IResource;
 
 public class OpenCLLanguage extends GCCLanguage implements ICLanguageKeywords, ILanguageSettingsProvider {
@@ -17,6 +19,8 @@ public class OpenCLLanguage extends GCCLanguage implements ICLanguageKeywords, I
 			"__global", "global", "__local", "local", "__constant", "constant", "__private", "private",
 			"__kernel", "kernel", "__read_only", "read_only", "__write_only", "write_only", "__read_write", "read_write" };
 	private String[] types = new String[] { "bool", "half" };
+	
+	private List<ICLanguageSettingEntry> settingEntries = new CopyOnWriteArrayList<ICLanguageSettingEntry>();
 
 	@Override
 	public Object getAdapter(Class adapter) {
@@ -54,8 +58,15 @@ public class OpenCLLanguage extends GCCLanguage implements ICLanguageKeywords, I
 	@Override
 	public List<ICLanguageSettingEntry> getSettingEntries(ICConfigurationDescription cfgDescription, IResource rc,
 			String languageId) {
-		// TODO Auto-generated method stub
-		return null;
+		if (settingEntries.isEmpty()) {
+			// fill cache
+			for (String keyword : keywords) {
+			}
+			for (String type : types) {
+				
+			}
+		}
+		return settingEntries;
 	}
 
 }
